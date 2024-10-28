7815b7c03d7906ba6eb04ee2ea875f27b6f6fbf2
dkanukov <dkanukov@ozon.ru>
2024-10-28 19:52:07 +0300

    fix aerospace

---
+++ resize-main-window.js
@@ -0,0 +1,43 @@
+const { exec, execSync } = require('child_process');
+const { readFile, writeFile } = require('fs');
+
+function update() {
+	const CASE_ZERO = 'outer.top =        [{monitor.main = 0}, 0]';
+	const CASE_NOT_ZERO = 'outer.top =        [{monitor.main = 32}, 0]';
+
+	try {
+		readFile('./aerospace.toml', (err, data) => {
+			if (err) {
+				console.log(err);
+			}
+
+			let cfg = data.toString();
+
+			if (cfg.includes(CASE_ZERO)) {
+				cfg = cfg.replace(CASE_ZERO, CASE_NOT_ZERO);
+			} else if (cfg.includes(CASE_NOT_ZERO)) {
+				cfg = cfg.replace(CASE_NOT_ZERO, CASE_ZERO);
+			} else {
+				console.log('Didn\'t find string');
+			}
+
+			writeFile('./aerospace.toml', cfg, (err) => { err && console.log(err) });
+		})
+	} catch (err) {
+		console.log(err);
+	}
+
+	execSync('aerospace reload-config', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+}
+
+update()
